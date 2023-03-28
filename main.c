f09059a7d4a6c00c1d24538332917419e9081648
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-03-28 17:20:54 -0600

    Operaciones aritmeticas con variables int

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 int main(void){
     int a=5;
     int b=2;
-    printf("%d\n",a);
-    printf("%d\n",b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
     return 0;
 }
