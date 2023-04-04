8056486342212fe5d9e71be26a432480597ffd68
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-04 14:39:36 -0600

    Funcion vacia que suma dos numeros

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
+void suma(void);//Declaracion de la funcion
+
 int main(void){
-    int a;
-    int b;
-    printf("Ingresa un valor para a ");
-    scanf("%d",&a);
-    printf("Ingresa un valor para b ");
-    scanf("%d",&b);
-    printf("%d\n",a+b);
-    printf("%d\n",a-b);
-    printf("%d\n",a*b);
-    printf("%d\n",a/b);
-    printf("%d\n",a%b);
+    suma();//Invocacion de funcion
     return 0;
+
+}
+
+void suma(void){//Definicion de la funcion
+    float a,b,c;
+    printf("Ingresa a ");
+    scanf("%f",&a);
+    printf("Ingresa b ");
+    scanf("%f",&b);
+    c=a+b;
+    printf("%f + %f = %f\n",a,b,c);
 }
