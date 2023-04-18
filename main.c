04f1d9e30e96592dacdb5b5ba2e187ca59eb76a6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-18 11:22:40 -0600

    Formula general con funciones con retorno y con argumentos

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+#include <math.h>
 
 float sumaDosNumeros(float x, float y);
 float calculaAreaRectangulo(float b, float h);
 float calculaPerimetroRectangulo(float b, float h);
+float calculaX1(float a, float b, float c);
+float calculaX2(float a, float b, float c);
 
 int main(void){
-    float base, altura,area,perimetro;
-
+    float a,b,c,x1,x2;
     //1.Pide datos
-    printf("Ingresa base ");
-    scanf("%f",&base);
-    printf("Ingresa altura ");
-    scanf("%f",&altura);
-
-    area=calculaAreaRectangulo(base, altura);
-    perimetro=calculaPerimetroRectangulo(base, altura);
-
+    printf("Ingresa a ");
+    scanf("%f",&a);
+    printf("Ingresa b ");
+    scanf("%f",&b);
+    printf("Ingresa c ");
+    scanf("%f",&c);
+    x1 = calculaX1(a,b,c);
+    x2 = calculaX2(a,b,c);
     //3.Muestra resultado
-    printf("area = %f\n",area);
-    printf("perimetro = %f\n",perimetro);
-
+    printf("x1 = %f\n",x1);
+    printf("x2 = %f\n",x2);
     return 0;
 }
 
@@ -37,4 +38,14 @@ float calculaPerimetroRectangulo(float b, float h){
     float p;
     p = 2*b + 2*h;
     return p;
+}
+float calculaX1(float a, float b, float c){
+    float x1;
+    x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);//2.Calcula formula
+    return x1;
+}
+float calculaX2(float a, float b, float c){
+    float x2;
+    x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);//2.Calcula formula
+    return x2;
 }
