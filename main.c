e137bca3ba3ecd30d55ce82b52e0929c7350a06b
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-23 07:02:48 -0600

    Estructura switch

--- main.c
+++ main.c
@@ -1,51 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-float sumaDosNumeros(float x, float y);
-float calculaAreaRectangulo(float b, float h);
-float calculaPerimetroRectangulo(float b, float h);
-float calculaX1(float a, float b, float c);
-float calculaX2(float a, float b, float c);
-
 int main(void){
-    float a,b,c,x1,x2;
-    //1.Pide datos
-    printf("Ingresa a ");
-    scanf("%f",&a);
-    printf("Ingresa b ");
-    scanf("%f",&b);
-    printf("Ingresa c ");
-    scanf("%f",&c);
-    x1 = calculaX1(a,b,c);
-    x2 = calculaX2(a,b,c);
-    //3.Muestra resultado
-    printf("x1 = %f\n",x1);
-    printf("x2 = %f\n",x2);
-    return 0;
-}
+    int opcion;
+    printf("Opcion 1\n");
+    printf("Opcion 2\n");
+    printf("Opcion 3\n");
+    printf("Cual opcion quieres? ");
+    scanf("%d",&opcion);
 
-float sumaDosNumeros(float x, float y){
-    float z;
-    z = x+y;//2.Calcula formula
-    return z;
-}
-float calculaAreaRectangulo(float b, float h){
-    float a;
-    a = b*h;
-    return a;
-}
-float calculaPerimetroRectangulo(float b, float h){
-    float p;
-    p = 2*b + 2*h;
-    return p;
-}
-float calculaX1(float a, float b, float c){
-    float x1;
-    x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);//2.Calcula formula
-    return x1;
-}
-float calculaX2(float a, float b, float c){
-    float x2;
-    x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);//2.Calcula formula
-    return x2;
+    switch(opcion){
+        case 1:
+            printf("\nSe ejecuta la opcion 1 =^)\n");
+            break;
+        case 2:
+            printf("\nSe ejecuta la opcion 2 =^)\n");
+            break;
+        case 3:
+            printf("\nSe ejecuta la opcion 3 =^)\n");
+            break;
+        default:
+            printf("\nSe ejecuta la opcion default =^)\n");
+            break;
+    }
+
+    return 0;
 }
